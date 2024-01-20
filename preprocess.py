b0f5bc4f64a6505a376cea66966acddc020a344b
Apollo1291 <hemphilled@icloud.com>
2024-01-19 22:58:54 -0500

    update notebook and preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,7 @@ from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 import torch
 
-def create_dataloader_for_test(csv_path, batch_size=64):
-   df = pd.read_csv(csv_path)
+def create_dataloader_for_test(df, batch_size=64):
    df = df.drop(['10'], axis=1)
    X = df.astype(float)
    X = torch.tensor(X.values, dtype=torch.float32)
@@ -11,12 +10,3 @@ def create_dataloader_for_test(csv_path, batch_size=64):
    batch_size = 32
    loader = DataLoader(dataset=dataset, batch_size=len(dataset), shuffle=False)
    torch.save(loader, 'dataloader.pth')
-
-import sys
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python preprocess.py <path_to_csv>")
-        sys.exit(1)
-
-    csv_path = sys.argv[1]
-    create_dataloader_for_test(csv_path)
